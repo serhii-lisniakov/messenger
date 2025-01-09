6b922a8e121de0924119371c6124378f85dce770
Serhii Lisniakov <serhiy.lisnyakov@gmail.com>
2025-01-09 11:15:57 +0200

    Updates

--- src/store/user/actions.js
+++
@@ -1,7 +0,0 @@
-export const authAction = (user) => async dispatch => {
-    localStorage.setItem('user', JSON.stringify(user))
-    dispatch ({
-        type: 'AUTH',
-        payload: user
-    })
-}
